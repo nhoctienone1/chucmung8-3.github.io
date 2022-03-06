cf1cc09c70a9a69123459802a66946195c978603
nhoctienone1 <92415626+nhoctienone1@users.noreply.github.com>
2022-03-06 14:09:12 +0700

    upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Bé vịt';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100009157854005';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng bé vịt';
+const contentCard = 'Chúc honey của anh 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
